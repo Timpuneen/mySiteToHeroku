cbcb74001cc2dff91e3924daee824acb11ef701e
Timpuneen <timpuh_work@mail.ru>
2021-05-22 09:09:12 +0600

    кажется нашел все-таки в чем ошибка

---
+++ static/js/bagfix.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    const lockPaddingValue = window.innerHeight + 'px';
+    $(".parallax").css("min-height", lockPaddingValue);
+    $(".parallax").css("height", lockPaddingValue);
+});
+
+$(window).resize(function() {
+    const lockPaddingValue = window.innerHeight + 'px';
+    $(".parallax").css("min-height", lockPaddingValue);
+    $(".parallax").css("height", lockPaddingValue);
+});
