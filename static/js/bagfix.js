89b85f1e8922ebadad24bf20dcac9f2ba8879e2e
Timpuneen <timpuh_work@mail.ru>
2021-05-22 17:59:35 +0600

    yep

--- static/js/bagfix.js
+++ static/js/bagfix.js
@@ -4,8 +4,11 @@
 //     $(".parallax").css("height", lockPaddingValue);
 // });
 
-// $(window).resize(function() {
-//     const lockPaddingValue = window.innerHeight + 'px';
-//     $(".parallax").css("min-height", lockPaddingValue);
-//     $(".parallax").css("height", lockPaddingValue);
-// });
+$(window).resize(function() {
+    let lockPaddingValue = window.innerHeight + 'px';
+    let para = $('.parallax').css('height');
+    let del = $('.parallax').css('background-size')
+        // $(".parallax").css("min-height", lockPaddingValue);
+        // $(".parallax").css("height", lockPaddingValue);
+    $('#startInfo').text(lockPaddingValue + para + del);
+});
