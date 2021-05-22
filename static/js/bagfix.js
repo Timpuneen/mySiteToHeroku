26b5c5edd8e2ab6d05537e9814fa57b38b2748f8
Timpuneen <timpuh_work@mail.ru>
2021-05-22 09:14:09 +0600

    yep

--- static/js/bagfix.js
+++ static/js/bagfix.js
@@ -3,9 +3,3 @@ $(document).ready(function() {
     $(".parallax").css("min-height", lockPaddingValue);
     $(".parallax").css("height", lockPaddingValue);
 });
-
-$(window).resize(function() {
-    const lockPaddingValue = window.innerHeight + 'px';
-    $(".parallax").css("min-height", lockPaddingValue);
-    $(".parallax").css("height", lockPaddingValue);
-});
