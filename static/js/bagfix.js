cbe534261c6ecd37ac3eb5be367cec49b807cba4
Timpuneen <timpuh_work@mail.ru>
2021-05-22 10:25:01 +0600

    yep

--- static/js/bagfix.js
+++ static/js/bagfix.js
@@ -1,9 +1,7 @@
-$(document).ready(function() {
-    const lockPaddingValue = window.innerHeight + 'px';
-    $(".parallax").css("min-height", lockPaddingValue);
-    $(".parallax").css("height", lockPaddingValue);
-});
-
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 // $(window).resize(function() {
 //     const lockPaddingValue = window.innerHeight + 'px';
 //     $(".parallax").css("min-height", lockPaddingValue);
