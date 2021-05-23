4826a69454dc2517197d430617fabe90004410d3
Timpuneen <timpuh_work@mail.ru>
2021-05-23 17:40:28 +0600

    yep

--- static/js/bagfix.js
+++ static/js/bagfix.js
@@ -1,17 +1,25 @@
-$(document).ready(function() {
-    const lockPaddingValue = window.outerHeight;
-    const hResult = String(lockPaddingValue * 1.2) + 'px';
-    const result = String(lockPaddingValue * 1.85) + 'px';
-    $(".parallax").css("min-height", hResult);
-    $(".parallax").css("height", hResult);
-    $(".parallax").css("background-size", result + "," + hResult)
-});
+// $(document).ready(function() {
+//     if (window.innerWidth < 992) {
+//         const lockPaddingValue = window.outerHeight;
+//         const hResult = String(lockPaddingValue * 1.2) + 'px';
+//         const result = String(lockPaddingValue * 1.85) + 'px';
+//         $(".parallax").css("min-height", hResult);
+//         $(".parallax").css("height", hResult);
+//         $(".parallax").css("background-size", result + "," + hResult)
+//     }
+// });
 
-$(window).resize(function() {
-    const lockPaddingValue = window.outerHeight;
-    const hResult = String(lockPaddingValue * 1.2) + 'px';
-    const result = String(lockPaddingValue * 1.85) + 'px';
-    $(".parallax").css("min-height", hResult);
-    $(".parallax").css("height", hResult);
-    $(".parallax").css("background-size", result + "," + hResult);
-});
+// $(window).resize(function() {
+//     if (window.innerWidth < 992) {
+//         const lockPaddingValue = window.outerHeight;
+//         const hResult = String(lockPaddingValue * 1.2) + 'px';
+//         const result = String(lockPaddingValue * 1.85) + 'px';
+//         $(".parallax").css("min-height", hResult);
+//         $(".parallax").css("height", hResult);
+//         $(".parallax").css("background-size", result + "," + hResult)
+//     } else {
+//         $(".parallax").css("min-height", "100vh")
+//         $(".parallax").css("height", "100vh")
+//         $(".parallax").css("background-size", "cover")
+//     }
+// });
