797c7f9773f84689aac956faee93da7841067cb7
Timpuneen <timpuh_work@mail.ru>
2021-05-23 16:39:10 +0600

    yep

--- static/js/bagfix.js
+++ static/js/bagfix.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     const lockPaddingValue = window.outerHeight;
     const hResult = String(lockPaddingValue * 1.2) + 'px';
-    const result = String(lockPaddingValue * 1.8) + 'px';
+    const result = String(lockPaddingValue * 1.85) + 'px';
     $(".parallax").css("min-height", hResult);
     $(".parallax").css("height", hResult);
     $(".parallax").css("background-size", result + "," + hResult)
 });
 
-// $(window).resize(function() {
-//     let lockPaddingValue = window.outerHeight + 'px';
-//     let para = $('.parallax').css('height');
-//     let del = $('.parallax').css('background-size')
-//         // $(".parallax").css("min-height", lockPaddingValue);
-//         // $(".parallax").css("height", lockPaddingValue);
-//     $('#startInfo').text(lockPaddingValue + para + del);
-// });
+$(window).resize(function() {
+    const lockPaddingValue = window.outerHeight;
+    const hResult = String(lockPaddingValue * 1.2) + 'px';
+    const result = String(lockPaddingValue * 1.85) + 'px';
+    $(".parallax").css("min-height", hResult);
+    $(".parallax").css("height", hResult);
+    $(".parallax").css("background-size", result + "," + hResult);
+});
